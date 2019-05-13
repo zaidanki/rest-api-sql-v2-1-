e75613825df5ebd945cfe5e1f4bae56d7df788cb
zaidanki <44997660+zaidanki@users.noreply.github.com>
2019-05-13 16:55:30 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# rest-api-sql-v2-1-Master
---
+++ routes.js
@@ -0,0 +1,183 @@
+const express = require('express');
+const router = express.Router();
+const Course = require("./models").Course;
+const User = require("./models").User
+const bcryptjs = require('bcryptjs')
+const auth = require('basic-auth')
+
+//Authenticator Building
+const authenticateUser = (req, res, next) => {
+    const credentials = auth(req);
+    let message = null;
+    if (credentials) {
+        // Finding the One email where it matches credentials that are provided
+        User.findOne({where: {emailAddress: credentials.name}}).then(
+            data => {
+                if (data) {
+                    // Use the bcryptjs npm package to compare the user's password
+                    const authenticated = bcryptjs
+                        .compareSync(credentials.pass, data.password);
+
+                    // If the passwords match...
+                    if (authenticated) {
+                        console.log(`Authentication successful for username: ${data.emailAddress}`);
+
+                        // Then store the retrieved user object on the request object
+                        // so any middleware functions that follow this middleware function
+                        // will have access to the user's information.
+                        req.currentUser = data;
+                    } else {
+                        message = `Authentication (password) failure for username: ${data.emailAddress}`;
+                    }
+                } else {
+                    message = `User not found for username: ${credentials.name}`;
+                }
+
+            }
+        ).catch(e => {
+            res.status(500).json({Error: e})
+        }).then( () => {
+            if (message) {
+                // Return a response with a 401 Unauthorized HTTP status code. and message
+                res.status(401).json({ message: message });
+            } else {
+                // Or if user authentication succeeded...
+                // Call the next() method.
+                next();
+            }
+        }
+    )}
+}
+
+router.use(express.urlencoded({ extended: true }));
+router.use(express.json());
+
+router.get('/', (req, res) => {
+    res.json({
+        message: 'Welcome MY API, Either add /users OR /courses',
+    });
+});
+
+
+// This is for users route
+router.get('/users', authenticateUser, (req, res) => {
+    //Basic Find All excluding some stuff
+    User.findAll({
+        attributes: { exclude: ["createdAt", "updatedAt", "password"] }
+    })
+        .then(data =>{
+            res.json(data)})
+        .catch(err => res.status(500).json(err));
+});
+
+router.post('/users', (req, res) => {
+    //finding user by id and then seeing if it exists, if not, function goes.
+    User.findByPk(req.body.id).then( user => {
+        if(!user){
+            const userNew = req.body
+            userNew.password = bcryptjs.hashSync(userNew.password)
+            User.create(userNew).then( () => {
+                res.status(201).end()
+            }).catch(e => {
+                // Validation
+                if(e.name === "SequelizeUniqueConstraintError" || "SequelizeValidationError"){
+                    res.status(400).json(e.errors)
+                } else {
+                    res.status(500).json({Error: e})
+                }})
+        } else {
+            res.status(403).json({Message: 'User Already Exists'})
+        }
+    }).catch(e => {
+        res.status(500).then({Error: e})
+    })
+});
+
+// This is for Courses Routes
+router.get('/courses', (req, res) => {
+    // finding all Courses
+    Course.findAll({
+        attributes: { exclude: ["createdAt", "updatedAt"] }
+    })
+        .then(data => res.json(data))
+        .catch(err => res.status(500).json(err));
+});
+
+
+// Get a single Course
+router.get('/courses/:id', (req, res) => {
+    Course.findByPk(req.params.id).then( book => {
+        if(book){
+            res.json(book)
+        } else {
+            res.status(404).json({message: 'Course not found'})
+
+        }
+    }).catch(error => {
+        res.send(500, error);
+    });
+});
+
+// Post a new Course
+router.post('/courses', authenticateUser , (req, res) => {
+    // checking if ID exists and if userID exists and then validating
+    Course.findByPk(req.body.id).then((course) => {
+        User.findByPk(req.body.userId).then( userId => {
+            if(course){
+                res.status(403).json({ message: 'This ID already exists'})
+            } else if(!userId) {
+                res.status(404).json({message: 'User Not Found'})
+            }else if(req.body){
+                Course.create(req.body).then(() => {
+                    res.status(201).end()
+                } ).catch(e => {
+                    // a couple of Validators
+                    if(e.name === "SequelizeValidationError" || "SequelizeUniqueConstraintError" ){
+                        res.status(400).json({Error: e.errors})
+                    }else {
+                    res.status(500).json({Error: e})
+                }})
+            }
+        })
+            .catch(e => {
+                res.status(500).json({Error: e})
+            })
+        } )
+        .catch(e => {
+            res.status(500).json({Error: e})
+    })
+});
+
+// Update an existing Course
+router.put('/courses/:id', authenticateUser , (req, res) => {
+    Course.findByPk(req.params.id).then( course => {
+        if(course){
+            course.update(req.body).then(res.status(201).end())
+        } else {
+            res.status(403).json({ message: 'Cant Update a course that does not exist'})
+        }
+    }).catch(e => {
+        res.status(500).json({Error: e})
+    })
+});
+
+// Delete a course
+router.delete('/courses/:id', authenticateUser , (req, res) => {
+    Course.findByPk(req.params.id).then( course => {
+        if(course){
+            course.destroy().then(res.status(201))
+                .catch(e => {
+                    res.status(500).json({Error: e})
+                })
+        } else {
+            res.status(403).json({ message: 'Cant Delete a course that does not exist'})
+        }
+    }).catch(e => {
+        res.status(500).json({Error: e})
+    })
+});
+
+
+
+
+module.exports = router
